282fdb4e42895280ee281ec137625e27494f9656
alex <alex.oliveiramartins05@gmail.com>
2025-01-14 22:21:23 -0300

    added tailwindcss

---
+++ pages/home/index.js
@@ -0,0 +1,5 @@
+function Home() {
+  return <h1 className="bg-blue-800">Agora foi?</h1>;
+}
+
+export default Home;
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,14 @@
+import { useRouter } from "next/router";
+import { useEffect } from "react";
+
 function Home() {
-  return "teste";
+  const router = useRouter();
+
+  useEffect(() => {
+    router.push("/home");
+  }, []);
+
+  return null;
 }
 
 export default Home;
